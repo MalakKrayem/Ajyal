8f3acef635419f2fb22fb7c26600580e1fbe31c8
Malak Krayem <70662779+MalakKrayem@users.noreply.github.com>
2022-10-03 21:34:39 +0300

    User CRUD

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|max:15',
+            'gender'=>'required|string|in:female,male',
+            'image'=>'mimes:jpg,png',
+            'phone'=>'numeric',
+            'overview' => 'string|max:255',
+            'position_description'=>'string|max:255'
+        ];
+    }
+}
